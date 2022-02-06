e0fc4d9481e852320c0f18a4f9ad0a9b4644fe0d
jaswiate <92920068+jaswiate@users.noreply.github.com>
2022-02-06 23:16:52 +0100

    Delete main directory

--- main/java/main/Tile.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-import java.awt.image.BufferedImage;
-
-public class Tile {
-    public BufferedImage image;
-    public boolean collision0 = false;
-    public boolean collision1 = false;
-}
